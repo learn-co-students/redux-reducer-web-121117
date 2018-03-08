219126ab1eeb0110996e71a45c1ef51b1c7d13bf
brianabaker <brianaclairbaker@gmail.com>
2018-03-08 13:53:44 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      let newFriends = state.friends.filter(friend => friend.id !== action.id)
+      return { friends: newFriends}
+      default:
+        return state;
+  }
 }
+
+
+// animals.slice(2)
+
+// let newFriends = state.friends.filter(f => f.id !== action.id);
+// +      return { friends: newFriends };
